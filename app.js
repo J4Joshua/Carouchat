a5789650ca1ef1b4c758310c948e1616d3f794bf
J4Joshua <j4jtyj@gmail.com>
2023-06-26 14:19:58 +0800

    added agent

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var telegramRouter = require('./routes/telegram');
+var agentRouter = require('./routes/agent');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/telegram', telegramRouter);
+app.use('/agent', agentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
